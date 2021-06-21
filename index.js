af41744473c7788374deb6fa4a85a8c6d7869c8d
Aniket Tapre <55484217+tapreaniket08@users.noreply.github.com>
2021-06-21 19:01:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+  var d= new Date();
+  var htime= d.getHours();
+  var mtime=d.getMinutes();
+  var stime =d.getSeconds(); 
+
+
+  var hrotation = 30*htime + mtime/2;
+  var mrotation = 6*mtime;
+  var srotation = 6*stime;
+
+
+  
+  hour.style.transform =`rotate(${hrotation}deg)`;
+  minute.style.transform =`rotate(${mrotation}deg)`;
+  secound.style.transform =`rotate(${srotation}deg)`;
+}, 1000);
